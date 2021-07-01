83b4c9f0c0f0b49c85c22b0ba0dd2a471da18f4a
Paul Michael Armstrong <paulmichael86@yahoo.com>
2021-07-01 08:19:56 -0700

    Fixes animation usability issue and simplifies script to remove multiple images

--- script.js
+++ script.js
@@ -1,19 +1,6 @@
 let ally = prompt('Do you support LGBTQ+ rights? (yes/no)')
 if(ally == 'yes'){
     document.write ('<img src="yes.png" class="confirm-image" id="hideMe">')
-}
-if(ally == 'Yes'){
-    document.write ('<img src="yes.png" class="confirm-image" id="hideMe">')
-}
-if(ally == 'YES'){
-    document.write ('<img src="yes.png" class="confirm-image" id="hideMe">')
-}
-if(ally == 'no'){
-    document.write ('<img src="no.png" class="confirm-image" id="hideMe">')
-}
-if(ally == 'No'){
-    document.write ('<img src="no.png" class="confirm-image" id="hideMe">')
-}
-if(ally == 'NO'){
+} else {
     document.write ('<img src="no.png" class="confirm-image" id="hideMe">')
 }
