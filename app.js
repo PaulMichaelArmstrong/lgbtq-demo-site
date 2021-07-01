7a721da3d5d824cfb0b936bc69bb54f87ec50aa0
Paul Michael Armstrong <paulmichael86@yahoo.com>
2021-07-01 12:11:54 -0700

    Converts JS to function; Updates case sensitivity of ally prompt

---
+++ app.js
@@ -0,0 +1,12 @@
+function allyPrompt() {
+    let ally = prompt('Do you support LGBTQ+ rights? (yes/no)').toLowerCase();
+    if(ally == 'yes'){
+        document.write ('<img src="yes.png" class="confirm-image" id="hideMe">')
+    } else if (ally=='no'){
+        document.write ('<img src="no.png" class="confirm-image" id="hideMe">')
+    } else {
+        document.write ('<img src="else.png" class="confirm-image" id="hideMe">')
+    }
+}
+
+allyPrompt();
--- script.js
+++
@@ -1,6 +0,0 @@
-let ally = prompt('Do you support LGBTQ+ rights? (yes/no)')
-if(ally == 'yes'){
-    document.write ('<img src="yes.png" class="confirm-image" id="hideMe">')
-} else {
-    document.write ('<img src="no.png" class="confirm-image" id="hideMe">')
-}
