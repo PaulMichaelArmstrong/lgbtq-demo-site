9ae67f1494ccae3cb5a3fdc97e0076080ebffdbb
Paul Michael Armstrong <paulmichael86@yahoo.com>
2021-07-01 00:40:26 -0700

    Adds prompt functionality

---
+++ script.js
@@ -0,0 +1,19 @@
+let ally = prompt('Do you support LGBTQ+ rights? (yes/no)')
+if(ally == 'yes'){
+    document.write ('<img src="yes.png" class="confirm-image" id="hideMe">')
+}
+if(ally == 'Yes'){
+    document.write ('<img src="yes.png" class="confirm-image" id="hideMe">')
+}
+if(ally == 'YES'){
+    document.write ('<img src="yes.png" class="confirm-image" id="hideMe">')
+}
+if(ally == 'no'){
+    document.write ('<img src="no.png" class="confirm-image" id="hideMe">')
+}
+if(ally == 'No'){
+    document.write ('<img src="no.png" class="confirm-image" id="hideMe">')
+}
+if(ally == 'NO'){
+    document.write ('<img src="no.png" class="confirm-image" id="hideMe">')
+}
