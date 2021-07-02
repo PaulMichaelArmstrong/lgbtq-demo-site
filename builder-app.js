851944c62d91524830a7b9761a4cbf0fa62e3421
Paul Michael Armstrong <paulmichael86@yahoo.com>
2021-07-02 00:48:58 -0700

    Adds build a flag page and functionality

---
+++ builder-app.js
@@ -0,0 +1,79 @@
+function builderColor1(){
+    let color = prompt('What is your top color 1/5').toLowerCase();
+    document.write('<section id=build-section style=background-color:'+(color)+'>')
+    return color;
+}
+
+builderColor1()
+
+function builderMeaning1(){
+    let meaning = prompt('What does it mean?');
+    document.write('<h2 class="build-text">' + meaning + '</h2></section>')
+    return meaning;
+}
+
+builderMeaning1()
+
+function builderColor2(){
+    let color = prompt('What is your next color? 2/5').toLowerCase();
+    document.write('<section id=build-section style=background-color:'+(color)+'>')
+    return color;
+}
+
+builderColor2()
+
+function builderMeaning2(){
+    let meaning = prompt('What does it mean?');
+    document.write('<h2 class="build-text">' + meaning + '</h2></section>')
+    return meaning;
+}
+
+builderMeaning2()
+
+function builderColor3(){
+    let color = prompt('What is your middle color? 3/5').toLowerCase();
+    document.write('<section id=build-section style=background-color:'+(color)+'>')
+    return color;
+}
+
+builderColor3()
+
+function builderMeaning3(){
+    let meaning = prompt('What does it mean?');
+    document.write('<h2 class="build-text">' + meaning + '</h2></section>')
+    return meaning;
+}
+
+builderMeaning3()
+
+function builderColor4(){
+    let color = prompt('What is your next color? 4/5').toLowerCase();
+    document.write('<section id=build-section style=background-color:'+(color)+'>')
+    return color;
+}
+
+builderColor4()
+
+function builderMeaning4(){
+    let meaning = prompt('What does it mean?');
+    document.write('<h2 class="build-text">' + meaning + '</h2></section>')
+    return meaning;
+}
+
+builderMeaning4()
+
+function builderColor5(){
+    let color = prompt('What is your bottom color? 5/5').toLowerCase();
+    document.write('<section id=build-section style=background-color:'+(color)+'>')
+    return color;
+}
+
+builderColor5()
+
+function builderMeaning5(){
+    let meaning = prompt('What does it mean?');
+    document.write('<h2 class="build-text">' + meaning + '</h2></section>')
+    return meaning;
+}
+
+builderMeaning5()
