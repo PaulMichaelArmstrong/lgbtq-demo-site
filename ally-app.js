c2aba0bd536e3bff601a71f50afeca5421e24406
Paul Michael Armstrong <paulmichael86@yahoo.com>
2021-07-02 09:28:49 -0700

    Fix yes/no confirmation

--- ally-app.js
+++ ally-app.js
@@ -1,16 +1,12 @@
 function allyPrompt() {
-    let userAnswer = prompt('Do you support LGBTQ+ rights? (yes/no)').toLowerCase();
-    while(userAnswer == 'yes' || userAnswer == 'no')
-    if(userAnswer == 'yes'){
-        document.write ('<img src="yes.png" class="confirm-image" id="hideMe">');
-        break;
-    } else if (userAnswer == 'no'){
-        document.write ('<img src="no.png" class="confirm-image" id="hideMe">');
-        break;
-    } else {
-        alert('Silly goose! It is a yes or no queston.');
+    let userAnswer = prompt('Do you support LGBTQ+ rights? (yes/no)');
+    while((userAnswer.toLowerCase() != 'yes') && (userAnswer.toLowerCase() != 'no')){
+        userAnswer = prompt('Do you support LGBTQ+ rights? (yes/no)');
     }
+    if(userAnswer.toLowerCase() == 'yes'){
+        document.write ('<img src="yes.png" class="confirm-image" id="hideMe">');
+    } else if (userAnswer.toLowerCase() == 'no'){
+        document.write ('<img src="no.png" class="confirm-image" id="hideMe">');
+    }
+ 
 }
-
-allyPrompt()
-
